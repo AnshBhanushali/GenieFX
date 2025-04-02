10125cc38a6914220661e2ff8310250b9ed1fc29
Ansh Bhanushali <bhanusad@mail.uc.edu>
2025-04-02 13:08:57 -0400

    created logger and user agent

--- scraper.py
+++ scraper.py
@@ -3,3 +3,13 @@ import requests
 from requests.exceptions import RequestException, Timeout
 from bs4 import BeautifulSoup
 from typing import List
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/58.0.3029.110 Safari/537.3"
+    )
+}
